9ec7a265bba36eddf64a147431f58491a4f8fb0d
João Alves <joao.alves@ecosis.com.br>
2022-03-15 10:50:59 -0300

    masked input

---
+++ src/components/TextMaskCustom/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MaskedInput, { MaskedInputProps } from "react-text-mask";
+
+// --------------- 𝕄𝕖𝕥𝕒𝕕𝕒𝕥𝕒 ---------------
+type MaskedInputCustomProps = MaskedInputProps | Readonly<MaskedInputProps>;
+
+/**
+ * Docs:
+ *
+ * - [ReactTextMask](https://github.com/text-mask/text-mask/tree/master/react/#readme)
+ */
+export const TextMaskCustom = React.forwardRef(
+  (props: MaskedInputCustomProps, ref) => (
+    <MaskedInput ref={undefined} placeholderChar={"\u2000"} {...props} />
+  )
+);
+
+export default TextMaskCustom;
