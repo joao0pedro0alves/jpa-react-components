fff992dc9587a1830176088abe458675724c1859
João Alves <joao.alves@ecosis.com.br>
2022-03-15 10:54:19 -0300

    adjust exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,11 @@
-// Components
 export { theme as darkTheme } from "./styles/theme/dark";
+
+// Core components
+export { Checkbox } from "./components/Checkbox";
 export { Table } from "./components/Table";
+export { TextField } from "./components/TextField";
+export { TextMaskCustom } from "./components/TextMaskCustom";
+
+// Form components
+export { useFormContext, useForm, FormContext, Form } from "./components/Form";
+export { FormTextField, FormCheckbox } from "./components/Form/components";
